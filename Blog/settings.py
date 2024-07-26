c59b051c800edbdd9ec67007095544f743de03f5
uditbaliyan <130930448+uditbaliyan@users.noreply.github.com>
2024-07-26 11:01:37 +0530

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -25,8 +25,11 @@ SECRET_KEY = 'django-insecure-it&g9oad!ao_f=1fhyl^+0#&(lcb+%s8@y#lbs!5%t+5dludr_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'blog-with-authentication.onrender.com',
+]
 
 # Application definition
 

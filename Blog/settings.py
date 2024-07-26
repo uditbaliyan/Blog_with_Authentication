2df1d7988324d7dcd1a1dc01ee316f09ac5ba74f
uditbaliyan <130930448+uditbaliyan@users.noreply.github.com>
2024-07-26 10:55:14 +0530

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'froala_editor',
+
     'markdownx',
     'markdownify',
     'django_markdown2',
